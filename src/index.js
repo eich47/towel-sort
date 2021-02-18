17b4ff9c2a03d21faa987e377097959f27952ffa
eich47 <eich@tut.by>
2021-02-18 16:45:29 +0300

    feat: add implementation on a functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length === 0) {
+        return [];
+    }
+
+    const result = [];
+    matrix.forEach((item, index) => {
+        if (index % 2 !== 0) {
+            item.reverse();
+            result.push(...item);
+        } else {
+            result.push(...item);
+        }
+    });
+    return result;
+};
